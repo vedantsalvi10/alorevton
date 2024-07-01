0d39bcb9aeba27dc5a751514a3c57405f4aa102a
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-07-01 20:11:42 +0530

    adding the canvas and making minute changes

---
+++ index.js
@@ -0,0 +1,7 @@
+function try_now(){
+  document.querySelector('.details')
+  .innerHTML = `<div class="product_details">
+  <video id="video" playsinline></video>
+    <canvas id="output"></canvas>
+    </div>`;
+}
